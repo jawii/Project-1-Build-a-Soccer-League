bb9737cb8292e38dc9cc4bdadc49915bf2f83bb4
jawii <kenttajaa@gmail.com>
2016-07-28 18:20:05 +0300

    Did the rest
    Must clean up a little

--- project.py
+++ project.py
@@ -1,5 +1,11 @@
 import csv
 import random
+from datetime import date
+from operator import itemgetter, attrgetter
+
+first_practise_day = "14th July"
+first_practise_time = "5 pm"
+
 #'Soccer Experience': 
 #'Height' (inches)': '44', 
 #'Guardian Name(s)': 'Aaron and Jill Finkelstein' 
@@ -24,41 +30,114 @@ def print_players(lst, team_name):
         print ("XP: ", player['Soccer Experience'])
         print ("Height (inches): ", player['Height (inches)'])
         print ("Guardian Name(s): ", player['Guardian Name(s)'])
+        if 'Team name' in player.keys():
+            print ("Team name: ", player['Team name'])
+
         print ("")
         
 def split_list(lst, parts):
-    return [lst[0::parts] for i in range(parts)]
-     
+    return [lst[i::parts] for i in range(parts)]
+
+def get_average_height(players):
+    total_height = 0
+    number_of_players = len(players)
+    for player in players:
+        total_height += int(player['Height (inches)'])
+    return total_height / float(number_of_players)
+
+def sort_by_height(players):
+    return sorted(players, key = itemgetter('Height (inches)'))
+
+#print_players(align_with_height(league), "League")
+
+
 def make_teams(league, num_teams):   
-    #take experienced players
+    #take experienced players away from league
     xpPlayers = []
+    league2 = league[:]
     for player in league:
         if player['Soccer Experience'] == 'YES':
-            xpPlayers.append(player) 
+            xpPlayers.append(player)
+            league2.remove(player)
+    league = league2
 
-    #move experienced players from league to end of list
+    #sort league and xpPlayers by height
+    xpPlayers = sort_by_height(xpPlayers)
+    league = sort_by_height(league)
+
+    #move experienced players back to league
     for player in xpPlayers:
-        league.remove(player)
-        league.append(player)
+        league.insert(0, player)
     #print_players(league)
     
     #make teams
     teams = split_list(league, num_teams)
-    
-    return (team for team in teams)
-    
-    
-raptors, dragons, sharks = make_teams(league, 3)
 
-#print_players(raptors, "raptors")
-#print_players(dragons, "dragons")
-#print_players(sharks, "sharks")
+    return tuple(team for team in teams)
+    
+
 
 def write_letter(player):
     letter = "Dear "
-    letter += player
+    letter += player['Guardian Name(s)'] + ". \n \n"
+
+    player["Team name"] = ""
+    if player in raptors:
+        player["Team name"] = "Raptors"
+    elif player in dragons:
+        player["Team name"] = "Dragons"
+    elif player in sharks:
+        player["Team name"] = "Sharks"
+
+    letter += "Your child " + player['Name'] + " " 
+    letter += "plays in team named " + player["Team name"] + "\n"
+    letter += "Our first practise will be in " \
+            + first_practise_day + " at " + first_practise_time + "."
 
     return letter
-print_players(league, "league")
-print (league)
 
+# Update teams to league
+for player in league:
+    if player in raptors:
+        player["Team name"] = "Raptors"
+    elif player in dragons:
+        player["Team name"] = "Dragons"
+    elif player in sharks:
+        player["Team name"] = "Sharks"
+
+
+if __name__ == "__main__":
+
+    dragons, raptors , sharks = make_teams(league, 3)
+
+    #print_players(dragons, "dragons")
+    #print_players(raptors, "raptors")
+    #print_players(sharks, "sharks")
+
+    league = list(dragons) + list(raptors) + list(sharks)
+
+    # Update teams to league
+    for player in league:
+        if player in raptors:
+            player["Team name"] = "Raptors"
+        elif player in dragons:
+            player["Team name"] = "Dragons"
+        elif player in sharks:
+            player["Team name"] = "Sharks"
+        else:
+            player["Team name"] = "(no team)"
+    
+    #print_players(league, "League")
+
+    for player in league:
+        file_name = player["Name"] + ".txt"
+        with open(file_name, "w") as letter:
+            letter.write(write_letter(player))
+
+#print_players(dragons, "dragons")
+#print_players(raptors, "raptors")
+#print_players(sharks, "sharks")
+
+#print (get_average_height(raptors))
+#print (get_average_height(dragons))
+#print (get_average_height(sharks))
