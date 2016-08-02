8205abcc6957abe293fbc0b1d6c5f8eb270d56fa
jawii <kenttajaa@gmail.com>
2016-08-02 23:10:49 +0300

    Started implementing game code

--- Project 2 - Battleship/board.py
+++ Project 2 - Battleship/board.py
@@ -1,172 +1,205 @@
 import constants
 import ships
 
+
 class Board(object):
-	"""Board class for battleship - game"""
-	
-	def __init__(self):
-		self.vertical_ship_mark = constants.VERTICAL_SHIP
-		self.horizontal_ship_mark = constants.HORIZONTAL_SHIP
-		self.miss_mark = constants.MISS
-		self.hit_mark = constants.HIT
-		self.sunk_mark = constants.SUNK
-		self.empty_mark = constants.EMPTY
-		self.board_size = constants.BOARD_SIZE
-		self.board = [[self.empty_mark for x in range(self.board_size)] for y in range(self.board_size)] 
-		self.ships = ships.ship_info.ships
+    """Board class for battleship - game"""
 
-	def print_board_heading(self):
-		return ("   " + " ".join([chr(c) for c in range(ord('A'), ord('A') + self.board_size)]))
+    def __init__(self):
+        self.vertical_ship_mark = constants.VERTICAL_SHIP
+        self.horizontal_ship_mark = constants.HORIZONTAL_SHIP
+        self.miss_mark = constants.MISS
+        self.hit_mark = constants.HIT
+        self.sunk_mark = constants.SUNK
+        self.empty_mark = constants.EMPTY
+        self.board_size = constants.BOARD_SIZE
+        self.board = [
+                [self.empty_mark for x in range(self.board_size)]
+                for y in range(self.board_size)
+                ]
+        self.ships = ships.ship_info.ships
 
-	def __str__(self):
+    def print_board_heading(self):
+        return ("   " + " ".join([chr(c)
+                for c in range(ord('A'), ord('A') + self.board_size)]))
 
-		string = ""
-		string += self.print_board_heading() + "\n"
-		row_num = 1
-		for row in self.board:
-			string += (str(row_num).rjust(2) + " " + (" ".join(row))) + "\n"
-			row_num += 1
-		
-		return string
-	
-	def clear_screen(self):
-		print("\033c", end="")
+    def __str__(self):
 
-	def ask_coordinates(self, ship):
+        string = ""
+        string += self.print_board_heading() + "\n"
+        row_num = 1
+        for row in self.board:
+            string += (str(row_num).rjust(2) + " " + (" ".join(row))) + "\n"
+            row_num += 1
+        return string
 
-			alpabets = [chr(c) for c in range(ord('a'), ord('a') + self.board_size)]
+    def clear_screen(self):
+        print("\033c", end="")
 
-			while True:
-				coordinate = input("Place the location (col, row) of the " + ship[0] + " (" + str(ship[1]) + " spaces): " )
-				coordinate = coordinate.replace(" ","")
-				coordinate = (coordinate[0], coordinate[1:])
-				#print(coordinate)
-				try:
-					row = int(coordinate[1])
-					if row not in list(range(self.board_size + 1)):
-						self.clear_screen()
-						print("")
-						print("Incorrect coordinate. Row number too high!")
-						print("")
-						print(board)
-						continue
-				except ValueError:
-					self.clear_screen()
-					print("")
-					print("Can't find the row. Type col first and then row. For Example 'a1'")
-					print("")
-					print(board)
-					continue
+    def ask_coordinates(self, ship):
 
-				if coordinate[0].lower() not in alpabets:
-					self.clear_screen()
-					print("")
-					print("Incorrect coordinate. Can't find the column. Type column first and then row.")
-					print("")
-					print(board)
-					continue
-				else:
-					break
-				
-			row = int(coordinate[1]) - 1
-			col = alpabets.index(coordinate[0])
+            alpabets = [
+                        chr(c) for c in
+                        range(ord('a'), ord('a') + self.board_size)
+                    ]
 
-			return (row, col)
+            while True:
+                coordinate = input(
+                        "Place the location (col, row) of the "
+                        + ship[0] + " (" + str(ship[1]) + " spaces): "
+                        )
+                coordinate = coordinate.replace(" ", "")
+                coordinate = (coordinate[0], coordinate[1:])
+                # print(coordinate)
+                try:
+                    row = int(coordinate[1])
+                    if row not in list(range(self.board_size + 1)):
+                        self.clear_screen()
+                        print("")
+                        print("Incorrect coordinate. Row number too high!")
+                        print("")
+                        print(self)
+                        continue
+                except ValueError:
+                    self.clear_screen()
+                    print("")
+                    print(
+                            "Can't find the row. "
+                            + "Type col first and then row. For Example 'a1'"
+                            )
+                    print("")
+                    print(self)
+                    continue
 
-	def ask_orient(self):
-			horizontal = True
-			is_horizontal= ""
-			while True:
-				is_horizontal = input("Is it horizontal? (Y)/N: ")
-				if is_horizontal.lower() == 'y':
-					horizontal = True
-					break
-				elif is_horizontal.lower() == 'n':
-					horizontal = False 
-					break
+                if coordinate[0].lower() not in alpabets:
+                    self.clear_screen()
+                    print("")
+                    print(
+                        "Incorrect coordinate."
+                        + "Can't find the column."
+                        + "Type column first and then row."
+                            )
+                    print("")
+                    print(self)
+                    continue
+                else:
+                    break
 
-			return horizontal
+            row = int(coordinate[1]) - 1
+            col = alpabets.index(coordinate[0])
 
-	def place_ship(self, row_col, ship_length, is_horizontal):
-		
-		row = row_col[0]
-		col = row_col[1]
+            return (row, col)
 
-		if is_horizontal:
-			mark = self.horizontal_ship_mark
-			for place in range(ship_length):
-				self.board[row][col + place] = mark
-		else:
-			mark = self.vertical_ship_mark
-			for place in range(ship_length):
-				self.board[row + place][col] = mark
+    def ask_orient(self):
+            horizontal = True
+            is_horizontal = ""
+            while True:
+                is_horizontal = input("Is it horizontal? (Y)/N: ")
+                if is_horizontal.lower() == 'y':
+                    horizontal = True
+                    break
+                elif is_horizontal.lower() == 'n':
+                    horizontal = False
+                    break
+
+            return horizontal
+
+    def place_ship(self, row_col, ship_length, is_horizontal):
+
+        row = row_col[0]
+        col = row_col[1]
+
+        if is_horizontal:
+            mark = self.horizontal_ship_mark
+            for place in range(ship_length):
+                self.board[row][col + place] = mark
+        else:
+            mark = self.vertical_ship_mark
+            for place in range(ship_length):
+                self.board[row + place][col] = mark
+
+    def can_ship_set(self, row_col, ship_length, is_horizontal):
+
+        error_string = "\n You can't place ship there.\n"
+        forbidden_places = [self.horizontal_ship_mark, self.vertical_ship_mark]
+        if row_col is None:
+            return (True, "")
+
+        else:
+            row = row_col[0]
+            col = row_col[1]
+            place = 0
+            if is_horizontal:
+                dummy_1 = col
+                dummy_2 = row
+                dummy_3 = col + place
+            else:
+                dummy_1 = row
+                dummy_2 = row + place
+                dummy_3 = col
+
+            is_space = dummy_1 <= self.board_size - ship_length
+            are_there_ships = False
+
+            try:
+                for place in range(ship_length):
+                    if self.board[dummy_2][dummy_3] in forbidden_places:
+                        are_there_ships = True
+                        error_string = (
+                            "\n You can't place ship there."
+                            + "There is already a ship. \n"
+                            )
+            except IndexError:
+                are_there_ships = False
+
+            if is_space and not are_there_ships:
+                return (False, "")
+            else:
+                return (True, error_string)
+
+    def set_ships(self, player_name):
+
+            for ship in self.ships:
+
+                row_col = None
+                is_horizontal = None
+                ship_length = None
+
+                can_set = self.can_ship_set(
+                        row_col,
+                        ship_length,
+                        is_horizontal
+                        )
+
+                while can_set[0]:
+                    print("Put the ships " + player_name)
+                    print(self)
+                    row_col = self.ask_coordinates(ship)
+                    is_horizontal = self.ask_orient()
+                    ship_length = ship[1]
+
+                    can_set = self.can_ship_set(
+                        row_col,
+                        ship_length,
+                        is_horizontal
+                        )
+
+                    if can_set[0]:
+                        self.clear_screen()
+                        print(can_set[1])
+
+                self.place_ship(row_col, ship_length, is_horizontal)
+                self.clear_screen()
+            #print(self)
+
+    def is_coord_in_board(self, coordinate):
+        ''' 
+        Checks if player coordinate input is in board
+        Return tuple of col and rows
+        '''
+        pass
 
 
-	def can_ship_set(self, row_col, ship_length,is_horizontal):
-		
-		error_string = "\n You can't place ship there.\n"
-		forbidden_places = [self.horizontal_ship_mark, self.vertical_ship_mark]
-		if row_col == None:
-			return (True, "")
-
-		else:
-			row = row_col[0]
-			col = row_col[1]
-			place = 0
-			if is_horizontal:
-				dummy_1 = col
-				dummy_2 = row
-				dummy_3 = col + place
-			else:
-				dummy_1 = row
-				dummy_2 = row + place
-				dummy_3 = col
-
-			is_space = dummy_1 <= self.board_size - ship_length
-			are_there_ships = False
-
-			try:
-				for place in range(ship_length):
-					if self.board[dummy_2][dummy_3] in forbidden_places:
-						are_there_ships = True
-						error_string = " \n You can't place ship there. There is already a ship. \n"
-			except IndexError:
-				are_there_ships = False
-
-			if is_space and not are_there_ships:
-				return (False, "")
-			else:
-				return (True, error_string)
-
-	def set_ships(self):
-		
-			for ship in self.ships:
-
-				row_col = None
-				is_horizontal = None
-				ship_length = None
-
-				can_set = self.can_ship_set(row_col, ship_length, is_horizontal)
-
-				while can_set[0]:
-					print(board)
-					row_col = self.ask_coordinates(ship)
-					is_horizontal = self.ask_orient()
-					ship_length = ship[1]
-					can_set = self.can_ship_set(row_col, ship_length, is_horizontal)
-					if can_set[0]:
-						self.clear_screen()
-						print(can_set[1])
-
-				self.place_ship(row_col, ship_length, is_horizontal)
-				self.clear_screen()
-			print(board)
-
-
-
-#board = Board()
-#print(board.board)
-#board.set_ships()
-
-
-		
+# board = Board()
+# print(board.board)
+# board.set_ships()
--- Project 2 - Battleship/game.py
+++ Project 2 - Battleship/game.py
@@ -1,26 +1,109 @@
 import constants
-from board import Board
+import board
 from ships import ShipInfo
 
 
 class Game(object):
-	def __init__(self):
-		self.player1_board = Board()
-		self.player2_board = Board()
+    def __init__(self):
+        self.player1_board = board.Board()
+        self.player1_guess_board = board.Board()
+        self.player2_board = board.Board()
+        self.player1_guess_board = board.Board()
 
-	def ask_names(self):
-		self.player1_name = input("Type Player 1 name: ")
-		self.player2_name = input("Type Player 2 name: ")
+    def ask_names(self):
+        self.player1_name = input("Type Player 1 name: ")
+        self.player2_name = input("Type Player 2 name: ")
+    
+    def clear_screen(self):
+        print("\033c", end="")
 
-	def start_game(self):
-		print("**************************")
-		print("Welcome to Battleship game")
-		self.ask_names()
+    def print_turn(self, player_name):
+        self.clear_screen()
 
-		player1_board.set_ships()
-		player2_board.set_ships()
+        turn_string = "----------------------------- \n"
+        turn_string += player_name +"'s" + " turn. \n" 
+        turn_string += "Press Enter to Continue \n"
+        turn_string += "----------------------------"
 
-Game().start_game()
+        while True:
+            print(turn_string)
+            continue_ = input("")
+            if continue_ == "":
+                break
+        self.clear_screen()
+
+    def check_win(self):
+        return (True, "")
+
+
+    def start_game(self):
+        self.clear_screen()
+        print("**************************")
+        print("")
+        print("Welcome to Battleship game")
+        print("")
+        print("**************************")
+        self.ask_names()
+
+        self.clear_screen()
+        self.print_turn(self.player1_name)
+        self.player1_board.set_ships(self.player1_name)
+        self.print_turn(self.player2_name)
+        self.player2_board.set_ships(self.player2_name)
+
+        while self.check_win()[0]:
+            
+            self.print_turn(self.player1_name)
+            attack(self.player2_board, self.player1_guess_board, self.player1_name)
+            self.clear_screen()
+            self.print_turn(self.player2_name)
+            attack(self.player1_board, self.player2_guess_board, self.player2_name)
+            self.clear_screen()
+
+
+
+def attack(board_to_attack, guess_board, player1_name):
+
+        turn = True
+
+        while turn:
+
+            print("Welcome " + player1_name)
+            print("Your Guess Board is below")
+            print(guess_board)
+
+            print("Your own Board is below")
+            print(board)
+            print("Give attack (col, row) coordinates: ")
+            attack_coordinate = input("--->")
+            
+            if is_valid_attack(attack_coordinate, board_to_attack):
+                # return if hit miss or invalid type
+                # update player2 board and own guess board
+                # turn = False
+                pass
+            else:
+                print("Not valid coordinate!")
+                continue
+
+
+
+def is_valid_attack(coordinate, enemy_board):
+    #
+    # check if coordinate is valid
+
+    # check if there is empty place in board at coordinate
+
+
+    return True
+
+
+
+
+
+
+game = Game()
+game.start_game()
 
 
 
